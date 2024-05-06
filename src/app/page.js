133481bcd4b3536a90b5a4241918c5abaf15197c
Boo-seon-woong <boosunung@gmail.com>
2024-05-06 21:55:39 +0900

    have fun with layout and page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>Hello, Nextjs!</>
+    <>
+    Hello, Nextjs!<br/>I'm not sure how this 'npm run build/start' gonna work, but we'll soon get it by this test lol<br/>now I got the difference with dev and build/start.<br/>
+    
+    <h2> Welcome</h2>
+    Hello, Next Js!
+    </>
     );
 }
