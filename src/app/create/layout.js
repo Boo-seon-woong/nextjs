375a6d24df665b5c364ef373a255161eea84aaa5
Boo-seon-woong <boosunung@gmail.com>
2024-05-22 21:17:19 +0900

    now we can write the new topic! thank you nextjs!

--- src/app/create/layout.js
+++
@@ -1,8 +0,0 @@
-export default function Layout(props){
-    return (
-        <form>
-            <h2>Layout inside parent's Layout</h2>
-            {props.children}
-        </form>
-    )
-}
--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,5 +1,30 @@
+"use client"
 export default function Create(){
     return (
-        <>create!</>
+        <form onSubmit={(e)=>{
+            e.preventDefault();
+            const title=e.target.title.value;
+            const body=e.target.body.value;
+            const options = {
+                method : 'POST',
+                headers : {
+                    'Content-Type': 'application/json'
+                },
+                body : JSON.stringify({title,body})
+            }
+            fetch('http://localhost:9999/topic',options)
+            .then(res=>res.json())
+            .then(result=>{console.log(result)});
+        }}>
+            <p>
+                <input type="text" name="title" placeholder="title"></input>
+            </p>
+            <p>
+                <input type="textarea" name="body" placeholder="body"></input>
+            </p>
+            <p>
+                <input type="submit" value="create"></input>
+            </p>
+        </form>
     )
 }
