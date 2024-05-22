9a0c4c0d01958413011195f7c513af9e4af5f060
Boo-seon-woong <boosunung@gmail.com>
2024-05-22 21:40:20 +0900

    i wanna deploy this page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output:'export',
+    assetPrefix:
+        process.env.NODE_ENV === "production"
+        ? "https://boo-seon-woong.github.io/nextjs" : "",
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    Hello, Nextjs!<br/>I'm not sure how this 'npm run build/start' gonna work, but we'll soon get it by this test lol<br/>now I got the difference with dev and build/start.<br/>
-    
-    <h2> Welcome</h2>
-    Hello, Next Js!
-    <br/>
+    <form>
     <img src="/moneybreath.jpg"></img>
-    </>
+    </form>
     );
 }
