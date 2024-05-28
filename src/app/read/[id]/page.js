24fdc2c61191c0eab075fd8e330bec6a17728588
Boo-seon-woong <boosunung@gmail.com>
2024-05-28 20:51:06 +0900

    이제빌드돌아간데제에에엔장난천재야

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,13 +1,22 @@
-export default async function Read(props){
-    const topic = await fetch(`http://localhost:9999/topic/${props.params.id}`).then(res=>res.json());
-    //const topic = await resp.json();
+// read/[id]/page.js
+
+// 정적 경로를 생성하는 함수 추가
+export async function generateStaticParams() {
+    // 예시: id가 1부터 10까지 있는 경로를 생성
+    const paths = Array.from({ length: 10 }, (_, i) => ({ id: (i + 1).toString() }));
+  
+    return paths;
+  }
+  
+  // 페이지 컴포넌트
+  export default async function Read(props) {
+    const topic = await fetch(`http://localhost:9999/topic/${props.params.id}`).then(res => res.json());
     const a = props.params.id;
     return (
-        <>
-            <h1>{a}</h1>
-            <h2>{topic.title}</h2>
-            artist : {topic.author}
-        </>
-    )
-}
-
+      <>
+        <h1>{a}</h1>
+        <h2>{topic.title}</h2>
+        artist: {topic.author}
+      </>
+    );
+  }
