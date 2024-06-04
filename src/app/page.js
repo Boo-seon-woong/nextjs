93e8523602d36927af525c102b3dfe7ecc0b2a59
Boo-seon-woong <boosunung@gmail.com>
2024-06-04 22:33:38 +0900

    color

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <form>
-    <img src="/moneybreath.jpg"></img>
+    <img src="/moneybreath.jpg" height={"400px"}></img>
     </form>
     );
 }
